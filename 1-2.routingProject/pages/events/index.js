d6bb58ee9182494371eac5a37b606645dc555efb
박현민 <dndb4902@naver.com>
2022-09-05 14:32:06 +0900

    # 슬러그페이지이동 로직

--- 1-2.routingProject/pages/events/index.js
+++ 1-2.routingProject/pages/events/index.js
@@ -1,4 +1,5 @@
 import {Fragment} from "react";
+import {useRouter} from "next/router";
 
 import {getAllEvents} from "../../dummy-data";
 import EventList from "../../components/events/event-list";
@@ -6,11 +7,18 @@ import EventsSearch from "../../components/events/events-search";
 
 const AllEventsPage = () => {
 
+    const router = useRouter();
     const events = getAllEvents();
+
+    const findEventsHandler = (year, month) => {
+        const fullPath =`/events/${year}/${month}`;
+
+        router.push(fullPath);
+    }
 
     return (
         <Fragment>
-            <EventsSearch />
+            <EventsSearch onSearch={findEventsHandler} />
             <EventList items={events}/>
         </Fragment>
     );
