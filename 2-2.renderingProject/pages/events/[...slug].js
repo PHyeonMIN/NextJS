5161f9ceee80a1652b463ee163c645336daaf72c
박현민 <dndb4902@naver.com>
2022-09-06 17:30:12 +0900

    # 서버사이드렌더링(SSR) 사용

--- 2-2.renderingProject/pages/events/[...slug].js
+++ 2-2.renderingProject/pages/events/[...slug].js
@@ -1,35 +1,28 @@
 import { Fragment } from 'react';
 import { useRouter } from 'next/router';
 
-import { getFilteredEvents } from '../../dummy-data';
+import { getFilteredEvents } from '../../helpers/api-util';
 import EventList from '../../components/events/event-list';
 import ResultsTitle from '../../components/events/results-title';
 import Button from '../../components/ui/button';
 import ErrorAlert from '../../components/ui/error-alert';
 
-function FilteredEventsPage() {
+function FilteredEventsPage(props) {
   const router = useRouter();
 
-  const filterData = router.query.slug;
+  // const filterData = router.query.slug;
+  //
+  // if (!filterData) {
+  //   return <p className='center'>Loading...</p>;
+  // }
+  //
+  // const filteredYear = filterData[0];
+  // const filteredMonth = filterData[1];
+  //
+  // const numYear = +filteredYear;
+  // const numMonth = +filteredMonth;
 
-  if (!filterData) {
-    return <p className='center'>Loading...</p>;
-  }
-
-  const filteredYear = filterData[0];
-  const filteredMonth = filterData[1];
-
-  const numYear = +filteredYear;
-  const numMonth = +filteredMonth;
-
-  if (
-    isNaN(numYear) ||
-    isNaN(numMonth) ||
-    numYear > 2030 ||
-    numYear < 2021 ||
-    numMonth < 1 ||
-    numMonth > 12
-  ) {
+  if (props.hasError) {
     return (
       <Fragment>
         <ErrorAlert>
@@ -42,10 +35,7 @@ function FilteredEventsPage() {
     );
   }
 
-  const filteredEvents = getFilteredEvents({
-    year: numYear,
-    month: numMonth,
-  });
+    const filteredEvents = props.events;
 
   if (!filteredEvents || filteredEvents.length === 0) {
     return (
@@ -60,7 +50,7 @@ function FilteredEventsPage() {
     );
   }
 
-  const date = new Date(numYear, numMonth - 1);
+  const date = new Date(props.date.year, props.date.month - 1);
 
   return (
     <Fragment>
@@ -68,6 +58,50 @@ function FilteredEventsPage() {
       <EventList items={filteredEvents} />
     </Fragment>
   );
+}
+
+export const getServerSideProps = async (context) => {
+    const {params} = context;
+
+    const filterData = params.slug;
+
+    const filteredYear = filterData[0];
+    const filteredMonth = filterData[1];
+
+    const numYear = +filteredYear;
+    const numMonth = +filteredMonth;
+
+    if (
+        isNaN(numYear) ||
+        isNaN(numMonth) ||
+        numYear > 2030 ||
+        numYear < 2021 ||
+        numMonth < 1 ||
+        numMonth > 12
+    ) {
+        return {
+            props: { hasError: true },
+            // notFound: true,
+            // redirect: {
+            //     destination: '/error'
+            // }
+        };
+    }
+
+    const filteredEvents = await getFilteredEvents({
+        year: numYear,
+        month: numMonth,
+    });
+
+    return {
+        props: {
+            events: filteredEvents,
+            date: {
+                year: numYear,
+                month: numMonth,
+            }
+        },
+    };
 }
 
 export default FilteredEventsPage;
