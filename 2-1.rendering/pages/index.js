553429acc15b004ea292c36b38ac3b8d9ebc892c
박현민 <dndb4902@naver.com>
2022-09-06 10:13:42 +0900

    # getStaticProps 설정옵션

--- 2-1.rendering/pages/index.js
+++ 2-1.rendering/pages/index.js
@@ -11,15 +11,28 @@ function HomePage(props) {
     );
 }
 
-export const getStaticProps = async () => {
+export const getStaticProps = async (context) => {
     const filePath = path.join(process.cwd(), 'data', 'dummy-backend.json');
     const jsonData = await fs.readFile(filePath);
     const data = JSON.parse(jsonData);
 
+    if(!data) {
+        return {
+            redirect: {
+                destination: '/no-data'
+            }
+        }
+    }
+
+    if(data.products.length ==0){
+        return {notFound:true};
+    }
+
     return {
         props: {
             products: data.products
-        }
+        },
+        revalidate: 10
     };
 }
 
