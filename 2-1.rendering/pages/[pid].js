b64c2827e5bb863e4ce2a66419c25e1387edbce7
박현민 <dndb4902@naver.com>
2022-09-06 11:22:15 +0900

    # fallback:false 후 404페이지

--- 2-1.rendering/pages/[pid].js
+++ 2-1.rendering/pages/[pid].js
@@ -5,9 +5,9 @@ import fs from 'fs/promises';
 const ProductDetailPage = (props) => {
     const {loadedProduct} = props;
 
-    // if(!loadedProduct){
-    //     return <p>Loading...</p>;
-    // }
+    if(!loadedProduct){
+        return <p>Loading...</p>;
+    }
 
     return (
         <Fragment>
@@ -33,6 +33,10 @@ export async function getStaticProps(context) {
     const data = await getData();
 
     const product = data.products.find(product => product.id === productId);
+
+    if(!product){
+        return {notFound: true};
+    }
 
     return {
         props: {
