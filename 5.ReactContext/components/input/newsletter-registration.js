6f896892d0b43949b133ffddcd3f04019b4a9224
박현민 <dndb4902@naver.com>
2022-09-08 09:35:31 +0900

    # 알림트리거&표시

--- 5.ReactContext/components/input/newsletter-registration.js
+++ 5.ReactContext/components/input/newsletter-registration.js
@@ -1,14 +1,22 @@
 import classes from './newsletter-registration.module.css';
 import {useRef, useContext} from "react";
+import NotificationContext from "../../store/notification-context";
 
 
 function NewsletterRegistration() {
     const emailInputRef = useRef();
+    const notificationCtx = useContext(NotificationContext);
 
   function registrationHandler(event) {
     event.preventDefault();
 
     const enteredEmail = emailInputRef.current.value;
+
+    notificationCtx.showNotification({
+        title:'Signing up...',
+        message:'Registering for newsletter',
+        status:'pending'
+    });
 
     fetch('/api/newsletter', {
         method: 'POST',
@@ -16,7 +24,27 @@ function NewsletterRegistration() {
         headers: {
             'Content-Type':'application/json'
         },
-    }).then((response) => response.json()).then((data)=>console.log(data));
+    }).then((response) => {
+        if(response.ok){
+            return response.json();
+        }
+
+        return response.json().then(data => {
+           throw new Error(data.message || 'Something went wrong!');
+        });
+    }).then((data)=>{
+        notificationCtx.showNotification({
+            title:'Success!',
+            message:'Successfully registered for newsletter!',
+            status:'success'
+        }).catch(error => {
+            notificationCtx.showNotification({
+                title:'Error!',
+                message: error.message || 'Something went wrong!',
+                status:'error'
+            });
+        });
+    });
   }
 
   return (
