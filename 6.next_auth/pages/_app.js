8b1124f46df29cacd3a483cf141e92bf365ab2fe
박현민 <dndb4902@naver.com>
2022-09-13 13:53:27 +0900

    # nextAuth Provider컴포넌트 사용

--- 6.next_auth/pages/_app.js
+++ 6.next_auth/pages/_app.js
@@ -1,11 +1,16 @@
+import { Provider } from "next-auth/client";
+
 import Layout from '../components/layout/layout';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+      // 세션 값을 미리 정의
+      <Provider session={pageProps.session}>    
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </Provider>
   );
 }
 
