128395f57ac9ecf2ec09e3e6ff12403467746e51
박현민 <dndb4902@naver.com>
2022-09-13 10:13:43 +0900

    # 로그인 기능

--- 6.next_auth/pages/api/auth/signup.js
+++ 6.next_auth/pages/api/auth/signup.js
@@ -2,27 +2,30 @@ import {connectToDatabase} from "../../../lib/db";
 import {hashPassword} from "../../../lib/auth";
 
 const handler = async (req, res) => {
-    const data = req.body;
+    if(req.method === 'POST') {
+        const data = req.body;
 
-    const {email, password} = data;
+        const {email, password} = data;
 
-    if (!email || !email.includes('@') || !password || password.trim().length < 7) {
-        res.status(422).json({message: 'Invalid input - password should also be at least 7 characters long.'});
-        return;
+        if (!email || !email.includes('@') || !password || password.trim().length < 7) {
+            res.status(422).json({message: 'Invalid input - password should also be at least 7 characters long.'});
+            return;
+        }
+
+        const client = await connectToDatabase();
+
+        const db = client.db();
+
+        const hashedPassword = await hashPassword(password);
+
+        const result = await db.collection('users').insertOne({
+            email:email,
+            password: hashedPassword
+        });
+
+        res.status(201).json({message: 'Created user!'});
     }
 
-    const client = await connectToDatabase();
-
-    const db = client.db();
-
-    const hashedPassword = hashPassword(password);
-
-    const result = await db.collection('users').insertOne({
-        email:email,
-        password: hashedPassword
-    });
-
-    res.status(201).json({message: 'Created user!'});
 };
 
 export default handler;
